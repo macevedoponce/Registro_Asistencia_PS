67329f74c5371023db85531b97858379bbf41f67
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-05-18 03:02:57 -0500

    Terminado 100%

--- app/src/main/java/com/acevedo/registroasistencia/Util/Util.java
+++ app/src/main/java/com/acevedo/registroasistencia/Util/Util.java
@@ -1,7 +1,7 @@
 package com.acevedo.registroasistencia.Util;
 
 public class Util {
-    public static final String RUTA = "https://trainingcrane.pe/estudiando/asistencia";
+    public static final String RUTA = "";
     public static final String RUTA_GRUPOS = RUTA + "/list_grupos.php";
     public static final String RUTA_PARTICIPANTES = RUTA + "/list_participantes.php";
     public static final String RUTA_REGISTRAR_ASISTENCIA = RUTA + "/add_asistencia.php";
